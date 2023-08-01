2be40b01d9bab78bb8f4f31bb93e256c238e6160
janupasilva2005 <janupasilva2005@gmail.com>
2023-08-01 20:28:35 +0530

    Initial commit

---
+++ client/src/Player/index.js
@@ -0,0 +1,19 @@
+/**
+ *  Player class implementation
+ */
+export class Player {
+  constructor(x, y, ctx) {
+    this.x = x
+    this.y = y
+    this.ctx = ctx
+    this.radius = 30
+    this.color = 'red'
+  }
+
+  draw() {
+    this.ctx.fillStyle = this.color
+    this.ctx.beginPath()
+    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+    this.ctx.fill()
+  }
+}
