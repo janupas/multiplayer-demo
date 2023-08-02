5b695ca722af698c58e387d9b8e188f8cac09121
janupasilva2005 <janupasilva2005@gmail.com>
2023-08-02 19:07:35 +0530

    Implemented the boilerplate game core

--- client/src/Core/index.js
+++ client/src/Core/index.js
@@ -3,20 +3,25 @@ import { Player } from '../Player'
 /**
  * This is the game core class
  *
+ * This is the actual heart of the game
+ * This will handle the main game loop
  * This is where it will render stuff to the screen
  */
 export class Core {
   constructor(cv, ctx) {
     this.cv = cv
     this.ctx = ctx
+    this.player = new Player(this.cv.width / 2, this.cv.height / 2, this.ctx)
   }
 
+  // This will run only once
   init() {
-    const player = new Player(this.cv.width / 2, this.cv.height / 2, this.ctx)
-    player.draw()
+    this.player.draw()
   }
 
+  // This is the main game loop
   update() {
-    console.log('GAME LOOP')
+    this.ctx.clearRect(0, 0, this.cv.width, this.cv.height)
+    this.player.draw()
   }
 }
