8b1eb5b1a3430bfd7c41e0509077af6267398549
janupasilva2005 <janupasilva2005@gmail.com>
2023-08-02 19:06:53 +0530

    Added boilerplate class instances

--- client/src/index.js
+++ client/src/index.js
@@ -6,5 +6,20 @@ const context = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-const game = new Game(canvas, context, 60)
+// Game constants
+const FPS = 60
+
+/**
+ * This is the main game object
+ *
+ * This setups the whole game
+ * Don't touch this unless something really bad happens
+ *
+ * @param canvas
+ * @param context
+ * @param FPS
+ */
+const game = new Game(canvas, context, FPS)
+
+// Starts the whole game
 game.start()
